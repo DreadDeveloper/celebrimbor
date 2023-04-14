f2b5ab0a568480a01d3d807d187863504ecfd0e8
Abhinav Kumar <kumaraka2000@gmail.com>
2023-04-14 14:07:20 +0530

    Initial commit

---
+++ src/main/java/com/talion/celebrimbor/Bike.java
@@ -0,0 +1,2 @@
+package com.talion.celebrimbor;public class Bike {
+}
---
+++ src/test/java/com/talion/celebrimbor/CelebrimborApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talion.celebrimbor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CelebrimborApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
